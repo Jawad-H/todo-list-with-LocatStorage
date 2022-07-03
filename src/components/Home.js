ac8c865c6133d304f41da46ba6ec8526a1baf56f
jawad hussain <jawwadhussain0@gmail.com>
2022-07-03 14:42:25 -0700

    Signed-off-by: jawad hussain <jawwadhussain0@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,10 @@
 import { React, useState, useEffect } from 'react'
 import styled from "styled-components";
 import { BsFillTrashFill } from "react-icons/bs";
+import useHook from './Add';
 function Home() {
     const [list, setlist, deleteElement, addTolocalStorage, input, setInput] = useHook();
 
-    const [list, setlist] = useState([]);
-    const [input, setInput] = useState("");
-
-    useEffect(() => {
-        if (localStorage.getItem("data")) {
-            setlist(JSON.parse(localStorage.getItem("data")));
-        }
-    }, []);
-
-    const addTolocalStorage = () => {
-        if (input.trim()) {
-            let newTodos = [...list, { todo: input.trim(), id: Date.now() }];
-            setlist(newTodos);
-            setInput("");
-            localStorage.setItem("data", JSON.stringify(newTodos));
-        }
-    };
-
-    const deleteElement = (id) => {
-        let newTodos = list.filter((todo) => todo.id !== id);
-        setlist(newTodos);
-        localStorage.setItem("data", JSON.stringify(newTodos));
-
-    };
 
     return (
         <Wrapper>
